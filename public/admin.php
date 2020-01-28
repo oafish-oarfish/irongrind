3b2e88a3efe18b1e8d09d76fea0399339dcf587e
Kevin Lanahan <klanahan24@yahoo.com>
2020-01-27 22:37:47 -0700

    finished admin page

--- public/admin.php
+++ public/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $ini = parse_ini_file('../config/config.ini.php');
 $servername = $ini['servername'];     // mydatabase
 $username = $ini['username'];     // myuser
@@ -11,63 +12,149 @@ $conn = new mysqli($servername, $username, $password,$databasename);
 
 // Check connection
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
-$sql = "SELECT * FROM Pricing_Promo"; $result = $conn->query($sql); $row =
-$result->fetch_assoc(); $promoText = $row["promoText"]; $weekPrice =
-$row["weekPrice"]; $twoWeekPrice = $row["twoWeekPrice"]; $monthPrice =
-$row["monthPrice"]; $twentySession = $row["twentySession"]; $fortySession =
-$row["fortySession"]; $sixtySession = $row["sixtySession"]; ?>
+$sql = "SELECT * FROM Pricing_Promo"; 
+$result = $conn->query($sql); 
+$row = $result->fetch_assoc(); 
+$promoText = $row["promoText"]; 
+$weekPrice = $row["weekPrice"]; 
+$twoWeekPrice = $row["twoWeekPrice"]; 
+$monthPrice = $row["monthPrice"]; 
+$twentySession = $row["twentySession"]; 
+$fortySession = $row["fortySession"]; 
+$sixtySession = $row["sixtySession"]; 
+
+if(isset($_POST['20Session']))
+{
+    $sql = "DELETE FROM Pricing_Promo";
+    if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+
+    $promoText = "";
+    if(isset($_POST['promoText'])){
+        $promoText = $_POST['promoText'];
+    }
+
+    $weekPrice = $_POST['weekly'];
+    $twoWeekPrice = $_POST['biweekly'];
+    $monthPrice = $_POST['monthly'];
+    $twentySession = $_POST['20Session'];
+    $fortySession = $_POST['40Session'];
+    $sixtySession = $_POST['60Session'];
+
+    $sql = "INSERT INTO Pricing_Promo (promoText, weekPrice, twoWeekPrice, monthPrice, twentySession, fortySession, sixtySession)
+    VALUES ('$promoText', '$weekPrice', '$twoWeekPrice', '$monthPrice', '$twentySession', '$fortySession', '$sixtySession')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "</br>New record created successfully";
+    } else {
+        echo "</br>Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
+
+?>
+
+
 
 <!DOCTYPE html>
 <html class="no-js" lang="en">
-  <head>
+
+<head>
     <link rel="stylesheet" href="css/bootstrap.min.css" />
-  </head>
+</head>
 
-  <body>
-  </br>
-</br>
+<body>
+    </br>
+    </br>
     <div class="row">
-      <div class="col-md-4"></div>
-      <div class="col-md-4">
-        <form>
-          <div class="form-group">
-            <label for="promoText">Promo Text</label>
-            <input
-              type="text"
-              class="form-control"
-              id="promoText"
-              placeholder="Blank if not current promotion"
-            />
-          </div>
-          <div class="form-group">
-            <label for="20Session">20 Session</label>
-            <input type="number" class="form-control" id="20Session" />
-          </div>
-          <div class="form-group">
-            <label for="40Session">40 Session</label>
-            <input type="number" class="form-control" id="40Session" />
-          </div>
-          <div class="form-group">
-            <label for="60Session">60 Session</label>
-            <input type="number" class="form-control" id="60Session" />
-          </div>
-          <div class="form-group">
-            <label for="weekly">Weekly</label>
-            <input type="number" class="form-control" id="weekly" />
-          </div>
-          <div class="form-group">
-            <label for="biweekly">Bi-Weekly</label>
-            <input type="number" class="form-control" id="biweekly" />
-          </div>
-          <div class="form-group">
-            <label for="monthly">Monthly</label>
-            <input type="number" class="form-control" id="monthly" />
-          </div>
+        <div class="col-md-4" style="margin-left: 1em;">
+            <a href="pricing.php">Back to pricing</a>
+        </div>
+        <div class="col-md-4">
+            <form action="admin.php" method="post" onsubmit="return validation()">
+                <div class="form-group">
+                    <label for="promoText">Promo Text</label>
+                    <input type="text" class="form-control" id="promoText" name="promoText"
+                        placeholder="Blank if not current promotion" />
+                </div>
+                <div class="form-group">
+                    <label for="20Session">20 Session - Current Price: <?php echo $twentySession?></label>
+                    <input type="number" class="form-control" name="20Session" id="20Session" />
+                </div>
+                <div class="form-group">
+                    <label for="40Session">40 Session - Current Price: <?php echo $fortySession?></label>
+                    <input type="number" class="form-control" name="40Session" id="40Session" />
+                </div>
+                <div class="form-group">
+                    <label for="60Session">60 Session - Current Price: <?php echo $sixtySession?></label>
+                    <input type="number" class="form-control" name="60Session" id="60Session" />
+                </div>
+                <div class="form-group">
+                    <label for="weekly">Weekly - Current Price: <?php echo $weekPrice?></label>
+                    <input type="number" class="form-control" name="weekly" id="weekly" />
+                </div>
+                <div class="form-group">
+                    <label for="biweekly">Bi-Weekly - Current Price: <?php echo $twoWeekPrice?></label>
+                    <input type="number" class="form-control" name="biweekly" id="biweekly"/>
+                </div>
+                <div class="form-group">
+                    <label for="monthly">Monthly - Current Price: <?php echo $monthPrice?></label>
+                    <input type="number" class="form-control" name="monthly" id="monthly"/>
+                </div>
 
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-      </div>
-      <div class="col-md-4"></div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div class="col-md-3"><?php echo "Current Promo Text: ".$promoText?></div>
     </div>
-  </body>
+</body>
+
+
+<script src="js/jquery-3.2.1.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script>
+
+    function validation(){
+
+        if($("#20Session").val() === ""){
+            alert("20 sessions is blank");
+            return false;
+        }
+
+        if($("#40Session").val() === ""){
+            alert("40 sessions is blank");
+            return false;
+        }
+
+        if($("#60Session").val() === ""){
+            alert("60 sessions is blank");
+            return false;
+        }
+
+        if($("#weekly").val() === ""){
+            alert("Weekly is blank");
+            return false;
+        }
+
+        if($("#biweekly").val() === ""){
+            alert("Biweekly is blank");
+            return false;
+        }
+
+        if($("#monthly").val() === ""){
+            alert("Monthly is blank");
+            return false;
+        }
+
+
+
+    }
+
+</script>
+
 </html>
